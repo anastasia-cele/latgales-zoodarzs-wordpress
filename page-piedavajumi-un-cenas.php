28ed70f233cfc797134ade6b35995602d385273d
Anastasija <anastasiatsel@yahoo.com>
2020-05-30 19:56:50 +0300

    Cenas

---
+++ page-piedavajumi-un-cenas.php
@@ -0,0 +1,205 @@
+<?php get_header(); ?>
+
+<div id="title">
+    <div class="title">
+        <h1>mūsu <br>bibliotēka</h1>
+        <div class="title-shadow">
+            <h1>mūsu <br>bibliotēka</h1>
+        </div>
+    </div>
+</div>
+
+<div id="piedavajumi">
+    <div class="item">
+        <div class="left">
+            <img src="http://localhost/wordpress/wp-content/themes/latgales-zoodarzs/public/img/cenas/1.png" alt="">
+            <div class="back"></div>
+        </div>
+        <div class="mid">
+            <div class="title">
+                <h2>Zoodārza apmeklēšana</h2>
+            </div>
+            <div class="text">
+                <span>Jūs varat apmeklēt zoodārzu no 10.00 līdz 18.00.</span>
+                <span>Nav apmeklēšanas: pirmdien, otrdien un Vāļsts svētkos. Šīs dienās notiek darbs ar
+                    dzīvniekiem.</span>
+            </div>
+        </div>
+        <div class="right">
+            <div class="cilveki">
+                <span>Skolēniem</span>
+                <span>Pieaugušajiem</span>
+                <span>Pirmsskolēniem</span>
+
+            </div>
+            <div class="cenas">
+                <span>30 centi</span>
+                <span>60 centi</span>
+                <span>bezmaksas</span>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="item">
+        <div class="left">
+            <img src="http://localhost/wordpress/wp-content/themes/latgales-zoodarzs/public/img/cenas/2.png" alt="">
+            <div class="back"></div>
+        </div>
+        <div class="mid">
+            <div class="title">
+                <h2>Ekskursijas skolēniem</h2>
+            </div>
+            <div class="text">
+                <span>Jūs varat pasūtīt ekskursiju pa t. 5426789.</span>
+                <span>Ekskursijas arī var būt tematiskas: "Dzīvnieku pielāgošanās dzīvei dabā", "Aizsargājamie
+                    dzīvnieki", "Eksotiskie dzīvnieki", "Abinieki", "Rāpuļi", "Pērtiķi" un citas.</span>
+            </div>
+        </div>
+        <div class="right">
+            <div class="cilveki">
+                <span>Skolotājs</span>
+                <span>Skolēniem</span>
+                <span>Pieaugušajiem</span>
+                <span>Ekskursiju vadītāja darbs</span>
+
+            </div>
+            <div class="cenas">
+                <span>bezmaksas</span>
+                <span>30 centi</span>
+                <span>60 centi </span>
+                <span>5 euro</span>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="item">
+        <div class="left">
+            <img src="http://localhost/wordpress/wp-content/themes/latgales-zoodarzs/public/img/cenas/3.png" alt="">
+            <div class="back"></div>
+        </div>
+        <div class="mid">
+            <div class="title">
+                <h2>Tematiskās stundas skolēniem</h2>
+            </div>
+            <div class="text">
+                <span>Tematiskās stundas skolēniem (bioloģija, dabas zinātne, ekoloģija).</span>
+                <span>Tēmas: "Dzīvnieku ekoloģija", "Dzīvnieku uzvedība", "Dzīvnieku aizsardzība", "Pielāgošanās videi",
+                    "Dzīvnieku vairošanas un attīstība" un citas.</span>
+            </div>
+        </div>
+        <div class="right">
+            <div class="cilveki">
+                <span>Skolotājs</span>
+                <span>Skolēniem </span>
+                <span>Ekskursiju vadītāja darbs</span>
+
+            </div>
+            <div class="cenas">
+                <span>bezmaksas</span>
+                <span>30 centi</span>
+                <span>5 euro</span>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="item">
+        <div class="left">
+            <img src="http://localhost/wordpress/wp-content/themes/latgales-zoodarzs/public/img/cenas/4.png" alt="">
+            <div class="back"></div>
+        </div>
+        <div class="mid">
+            <div class="title">
+                <h2>Tematiskās ekskursijas, lekcijas studentiem</h2>
+            </div>
+            <div class="text">
+                <span>Tematiskās ekskursijas, lekcijas studentiem (bioloģija, vides zinātne, ekoloģija,
+                    biotehnoloģijas).</span>
+                <span>Tēmas: "Dzīvnieku ekoloģijas īpatnības", "Dzīvnieku etoloģijas īpatnības", "Pielāgošanās videi",
+                    "Bioloģiskas daudzveidības saglabāšana ex-situ" un citas.</span>
+            </div>
+        </div>
+        <div class="right">
+            <div class="cilveki">
+                <span>Pasniedzējs</span>
+                <span>Studentiem</span>
+                <span>Ekskursiju vadītāja darbs</span>
+
+            </div>
+            <div class="cenas">
+                <span>bezmaksas</span>
+                <span>30 centi</span>
+                <span>5 euro</span>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="item">
+        <div class="left">
+            <img src="http://localhost/wordpress/wp-content/themes/latgales-zoodarzs/public/img/cenas/5.png" alt="">
+            <div class="back"></div>
+        </div>
+        <div class="mid">
+            <div class="title">
+                <h2>Zooloģiskais tālrunis</h2>
+            </div>
+            <div class="text">
+                <span>Mēs sniedzam konsultācijas eksotisko dzīvnieku (čūskas, bruņurupuči, zivtiņas, vardes, kukaiņi)
+                    mājās uzturēšanas jautājumos pa t.5426789 vai 29621191.</span>
+            </div>        
+        </div>
+        <div class="right">
+            <div class="bezmaksas">
+                <span>bezmaksas</span>
+            </div>
+        </div>
+    </div>
+
+    <div class="item">
+        <div class="left">
+            <img src="http://localhost/wordpress/wp-content/themes/latgales-zoodarzs/public/img/cenas/6.png" alt="">
+            <div class="back"></div>
+        </div>
+        <div class="mid">
+            <div class="title">
+                <h2>Eksotisko dzīvnieku pieņemšana</h2>
+            </div>
+            <div class="text">
+                <span>Dažreiz cilvēks nevar vairs turēt mājās savu eksotisko dzīvnieku. Mēs pieņemam eksotiskus
+                    dzīvniekus (čūskas, bruņurupuči, papagaiļi, pērtiķi, žurkas, kāmīši, jūras cūciņas) mūsu
+                    zoodārzā.</span>
+            </div>        
+        </div>
+        <div class="right">
+            <div class="bezmaksas">
+                <span>bezmaksas</span>
+            </div>
+        </div>
+    </div>
+
+    <div class="item">
+        <div class="left">
+            <img src="http://localhost/wordpress/wp-content/themes/latgales-zoodarzs/public/img/cenas/7.png" alt="">
+            <div class="back"></div>
+        </div>
+        <div class="mid">
+            <div class="title">
+                <h2>Čūsku ķeršana</h2>
+            </div>
+            <div class="text">
+                <span>Tikai Daugavpilī. Ja Jūs satiekat odzi vai citu čūsku Daugavpilī (parkā, pagalmā), piezvaniet pa
+                    t.29621191, mēs atbrauksim un palīdzēsim.</span>
+            </div>        
+        </div>
+        <div class="right">
+            <div class="bezmaksas">
+                <span>bezmaksas</span>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
