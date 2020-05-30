59469b6946b2e285cc4c2eab2b7f46aa77609c48
Anastasija <anastasiatsel@yahoo.com>
2020-05-30 17:44:23 +0300

    Biblioteka

---
+++ page-petnieciska-darbiba.php
@@ -0,0 +1,85 @@
+<?php get_header(); ?>
+
+<div id="title">
+    <div class="title">
+        <h1>mūsu <br>bibliotēka</h1>
+        <div class="title-shadow">
+            <h1>mūsu <br>bibliotēka</h1>
+        </div>
+    </div>
+</div>
+
+<div id="biblioteka">
+    <div class="gramatas">
+        <div class="title">
+            <h2>grāmatas</h2>
+        </div>
+        <div class="content">
+            <a href="" download>Pupiņš M., Pupiņa A. (2011): Latvijas pieaugušo abinieku
+                sugu lauku noteicējs. Daugavpils Universitāte, Akadēmiskais apgāds "Saule": 1-76. ISBN
+                978-9984-14-531-0</a>
+            <a href="" download>Pupiņš M., Pupiņa A. (2011): Latvijas pieaugušo abinieku sugu lauku noteikšanas
+                shēma.
+                [A field guide schema to the adult Amphibians species of Latvia]. -Daugavpils universitāte, DZMF: 2
+                lp.</a>
+            <a href="" download>Pupiņš M., Pupiņa A., Škute A. (2010): Vides un biotopu plānošana Eiropas purva
+                bruņurupuču Emys orbicularis saglabāšanai Latvijā [Planning of environment and biotopes for the
+                conservation of Emys orbicularis in Latvia]. -Daugavpils Universitāte: 1-192. (In Latvian).</a>
+            <a href="" download>Pupiņš M., Pupiņa A. (2007): Eiropas purva bruņurupucis Emys orbicularis (Linnaeus,
+                1758) un tā aizsardzība Latvijā. [European pond turtle Emys orbicularis (Linnaeus, 1758) and
+                conservation of it in Latvia]. -Latgales ekoloģiskā biedrība: 1-162. (In Latvian).</a>
+            <a href="" download>Pupiņa A., Pupiņš M. (2007): Sarkanvēdera ugunskrupis Bombina bombina (Linnaeus,
+                1761)
+                un tā aizsardzība Latvijā. [The fire-bellied toad Bombina bombina (Linnaeus, 1761) and conservation
+                of
+                it in Latvia]. - Latgales ekoloģiskā biedrība: 1-143. (In Latvian).</a>
+            <a href="" download>Līdaka I., Pupiņš M., Leimane D. (2005): Meklējam purva bruņrupuci Latvijā. [We look
+                for
+                pond turtle in Latvia]. -Rīgas nacionālais zooloģiskais dārzs: 1-2. (In Latvian).</a>
+        </div>
+    </div>
+    <div class="disertacijas">
+        <div class="title">
+            <h2>disertācijas</h2>
+        </div>
+        <div class="content">
+            <a href="" download>A.Pupiņa (2011): Sarkanvēdera ugunskrupju Bombina bombina L. ekoloģijas īpatnības uz
+                sugas areāla ziemeļu robežas Latvijā. Dr.biol. Promocijas darbs. -Daugavpils Universitāte: 1-131.</a>
+            <a href="" download>
+                Pupiņa A. (2011): Sarkanvēdera ugunskrupju Bombina bombina L. ekoloģijas īpatnības uz sugas areāla
+                ziemeļu robežas Latvijā. Dr.biol. promocijas darba kopsavilkums. -Daugavpils Universitāte: 1-61.</a>
+            <a href="" download>Pupiņš M. (2009): Eiropas purva bruņurupucis Emys orbicularis (Linnaeus, 1758) un tā
+                ekoloģijas aspekti uz sugas areāla ziemeļu robežas Latvijā. Dr.biol. promocijas darbs. Latvija,
+                Daugavpils Universitāte: 1-145.</a>
+            <a href="" download>
+                Pupiņš M. (2009): Eiropas purva bruņurupucis Emys orbicularis (Linnaeus, 1758) un tā ekoloģijas aspekti
+                uz sugas areāla ziemeļu robežas Latvijā. Dr.biol. promocijas darba kopsavilkums. -Latvija, Daugavpils
+                Universitāte: 1-71. ISBN 978-9984-14-468-9</a>
+        </div>
+    </div>
+    <div class="plani">
+        <div class="title">
+            <h2>sugu aizsardzības plāni</h2>
+        </div>
+        <div class="content">
+            <a href="" download>Pupiņš M., Pupiņa A. (2007): Eiropas purva bruņurupuča Emys orbicularis (Linnaeus, 1758)
+                sugas aizsardzības plāns Latvijā. -Dabas aizsardzības pārvalde, Rīga: 1-104.</a>
+            <a href="" download>Pupiņš M., Pupiņa A. (2006): Sarkanvēdera ugunskrupja Bombina bombina (Linnaeus, 1761)
+                sugas aizsardzības plāns Latvijā. -Dabas aizsardzības pārvalde, Rīga: 1-82.</a>
+        </div>
+    </div>
+    <div class="materiali">
+        <div class="title">
+            <h2>materiāli</h2>
+        </div>
+        <div class="content">
+            <a href="" download>Latgales Zoodārza Hronika 2007.gads</a>
+            <a href="" download>Latgales Zoodārza Hronika 2008.gads</a>
+            <a href="" download>Latgales Zoodārza Hronika 2009.gads</a>
+            <a href="" download>Latgales Zoodārza Hronika 2010.gads</a>
+            <a href="" download>Latgales Zoodārza Hronika 2011.gads</a>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
